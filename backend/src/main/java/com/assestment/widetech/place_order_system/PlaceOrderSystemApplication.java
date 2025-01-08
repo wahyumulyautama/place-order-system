7d624ad902d9e5c0b149cd35214cb5d02d06079c
wahyumulyautama <wahyumulyautama@gmail.com>
2025-01-09 00:13:53 +0700

    Initial Commit

---
+++ backend/src/main/java/com/assestment/widetech/place_order_system/PlaceOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.assestment.widetech.place_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceOrderSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/assestment/widetech/place_order_system/PlaceOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assestment.widetech.place_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
